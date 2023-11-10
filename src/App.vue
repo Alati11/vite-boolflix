8bbf04930ff16b39cab98f9564fae82ce164aae9
Alati11 <peppealati@gmail.com>
2023-11-10 15:30:49 +0100

    add component header

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script >
+
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+
+
+
+
+<template>
+  <AppHeader />
+
+</template>
+
+<style>
+* {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+
+}
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'AppHeader',
+            message: 'BOOLFLIX',
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="header-container">
+        <div class="header">
+            {{ message }}
+        </div> 
+        <div>
+            <input class="header-input" type="text" placeholder="Search">
+        </div>  
+    </div>
+    
+</template>
+
+<style scoped>
+
+
+.header-container {
+    background-color: black;
+    color: red;
+    width: 100%;
+    height: 80px;
+    margin: 0px;
+    padding: 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.header-input {
+
+}
+
+
+
+</style>
+
+
